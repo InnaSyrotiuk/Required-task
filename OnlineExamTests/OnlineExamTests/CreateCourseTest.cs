bec1936bdc9b539722a68421316f083480bef609
Inna <inna.syrotiuk@gmail.com>
2018-08-12 13:24:14 +0300

    Create tests: CreateCourse and DeleteCourse

---
+++ OnlineExamTests/OnlineExamTests/CreateCourseTest.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExamTests
+{
+    public class CreateCourseTest
+    {
+
+        public static string BaseUrl = "http://localhost:55842/";
+        IWebDriver driver = new ChromeDriver();
+
+        [Test]
+        public void CreateCourseTet()
+        {
+            driver.Navigate().GoToUrl(BaseUrl);
+            var loginButton = driver.FindElement(By.XPath("//a[@href='/Account/Login']"));
+            loginButton.Click();
+            var inpuEmail = driver.FindElement(By.XPath("//*[contains(@id,'emailLogin')]"));
+            inpuEmail.SendKeys("teacher@gmail.com");
+            var inputPassword = driver.FindElement(By.XPath("//*[contains(@id,'passwordLogin')]"));
+            inputPassword.SendKeys("Teacher_123");
+            var signInButton = driver.FindElement(By.XPath("//*[@value='Sign In']"));
+            signInButton.Click();
+
+            var navBarElement = driver.FindElement(By.XPath("//*[@id='gn-menu']//a[contains (@class, 'gn-icon-menu')]"));
+            navBarElement.Click();
+
+            var courseButton = driver.FindElement(By.XPath("//a[@href='/CourseManagement']"));
+            courseButton.Click();
+
+            var addCourseButtton = driver.FindElement(By.XPath("//a[@href='/CourseManagement/Create']"));
+            addCourseButtton.Click();
+
+            var newCourseName = "New Course" + Guid.NewGuid().ToString();
+
+            var inputCourseName = driver.FindElement(By.Id("Name"));
+            inputCourseName.SendKeys(newCourseName);
+
+            var inputCourseDescription = driver.FindElement(By.XPath("//*[contains(@id, 'Description')]"));
+            inputCourseDescription.SendKeys("This course for beginners");
+
+            var saveButton = driver.FindElement(By.XPath("//*[contains(@value, 'Ok')]"));
+            saveButton.Click();
+        
+            var savedCourse = driver.FindElement(By.XPath($"//*[text()='{newCourseName}']"));
+            var result = savedCourse.Text;
+            Assert.AreEqual(newCourseName, result, "New course not found");
+                   
+        }
+    }
+}
+
---
+++ OnlineExamTests/OnlineExamTests/DeleteCourse.cs
@@ -0,0 +1,88 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExamTests
+{
+    public class DeleteCourse
+    {
+        public static string BaseUrl = "http://localhost:55842/";
+        IWebDriver driver = new ChromeDriver();
+
+        [Test]
+        public void DeleteCourseTest()
+        {
+            driver.Navigate().GoToUrl(BaseUrl);
+            var loginButton = driver.FindElement(By.XPath("//a[@href='/Account/Login']"));
+            loginButton.Click();
+            var inpuEmail = driver.FindElement(By.XPath("//*[contains(@id,'emailLogin')]"));
+            inpuEmail.SendKeys("teacher@gmail.com");
+            var inputPassword = driver.FindElement(By.XPath("//*[contains(@id,'passwordLogin')]"));
+            inputPassword.SendKeys("Teacher_123");
+            var signInButton = driver.FindElement(By.XPath("//*[@value='Sign In']"));
+            signInButton.Click();
+
+            var navBarElement = driver.FindElement(By.XPath("//*[@id='gn-menu']//a[contains (@class, 'gn-icon-menu')]"));
+            navBarElement.Click();
+
+            var courseButton = driver.FindElement(By.XPath("//a[@href='/CourseManagement']"));
+            courseButton.Click();
+
+            var myCourseButton = driver.FindElement(By.XPath("//a[@href='/CourseManagement/ViewCourses']"));
+            myCourseButton.Click();
+
+            bool isDeleteVerified = false;
+            var listCourses = driver.FindElements(By.XPath("//*[@class='table']//tr"));
+            var cousesExist = listCourses != null && listCourses.Any();
+            Assert.True(cousesExist, "Not found any courses");
+            if (listCourses != null)
+            {
+
+                foreach (IWebElement row in listCourses)
+                {
+                    try
+                    {
+                        var nameElement = row.FindElement(By.XPath(".//*[text()='New Courseb76eda36-02b0-4968-acd7-a881f01df4eb']"));
+                        if (nameElement != null)
+                        {
+                            var deleteButton = row.FindElement(By.XPath(".//a[text()='Delete']"));
+                            deleteButton.Click();
+                        }
+                    }
+                    catch (Exception e)
+                    {
+
+                    }
+                }
+
+            }
+
+            listCourses = driver.FindElements(By.XPath("//*[@class='table']//tr"));
+            foreach (IWebElement row in listCourses)
+            {
+                try
+                {
+                    var nameElement = row.FindElement(By.XPath(".//*[text()='New Courseb76eda36-02b0-4968-acd7-a881f01df4eb']"));
+                    if (nameElement != null)
+                    {
+                        var recoverButton = row.FindElement(By.XPath(".//a[text()='Recover']"));
+                        var result = recoverButton.Text;
+                        StringAssert.AreEqualIgnoringCase("Recover", result, "Delete is not work");
+                        isDeleteVerified = true;
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+            Assert.True(isDeleteVerified, "Courses not deleted");
+
+        }
+    }
+}
